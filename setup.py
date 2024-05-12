bf0bb8f4be973cc17fcd4c313910bcef7f882fac
rasikanagolkar <68282418+rasikanagolkar@users.noreply.github.com>
2024-05-12 19:39:12 +0530

    Added unit testcases

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup,find_packages
+
+setup(name='xetra_project',version='1.0',packages=find_packages())
--- xetra/common/s3.py
+++ xetra/common/s3.py
@@ -24,7 +24,6 @@ class S3BucketConnector():
         self.endpoint_url = endpoint_url
         self.session = boto3.Session(aws_access_key_id = os.environ[access_key],
                                      aws_secret_access_key = os.environ[secret_key])
-        
         self._s3 = self.session.resource(service_name='s3',endpoint_url=endpoint_url)
         self._bucket = self._s3.Bucket(bucket)
 
