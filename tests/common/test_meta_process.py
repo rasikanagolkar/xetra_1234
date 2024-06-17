d4175034adb9fc6acd28b57d1cbbc9ed4564bc7e
rasikanagolkar <68282418+rasikanagolkar@users.noreply.github.com>
2024-06-17 19:39:34 +0530

    Added meta process code

--- tests/common/test_meta_process.py
+++ tests/common/test_meta_process.py
@@ -56,3 +56,199 @@ class TestMetaProcessMethods(unittest.TestCase):
     def tearDown(self):
         # mocking s3 connection stop
         self.mock_s3.stop()
+
+
+    def test_update_meta_file_no_meta_file(self):
+        """
+            Tests the update_meta_file when there is no meta file
+        """
+        #print('test_update_meta_file_no_meta_file')
+        date_list_exp = ['2021-04-16','2021-04-17']
+        proc_date_list_exp = [datetime.today().date()] * 2
+
+        # Test init
+        meta_key = 'meta.csv'
+        # Method execution
+        MetaProcess.update_meta_file(date_list_exp,meta_key,self.s3_bucket_meta)
+        # Read meta file
+        data = self.s3_bucket.Object(key=meta_key).get().get('Body').read().decode('utf-8')
+        out_buffer = StringIO(data)
+        df_meta_result = pd.read_csv(out_buffer)
+        date_list_result =list(df_meta_result[MetaProcessFormat.META_SOURCE_DATE_COL.value])
+        proc_date_list_result =list(
+            pd.to_datetime(df_meta_result[MetaProcessFormat.META_PROCESS_COL.value]).dt.date
+        )
+
+        #Test after method execution
+        self.assertEqual(date_list_exp, date_list_result)
+        self.assertEqual(proc_date_list_exp,proc_date_list_result)
+
+        #cleanup after test
+        self.s3_bucket.delete_objects(
+            Delete={
+                'Objects':[
+                    {
+                        'Key': meta_key
+                    }
+                ]
+            }
+        )
+
+
+    def test_update_meta_file_empty_date_list(self):
+        """
+            Tests the update_meta_file when theargument extract_date_list is empty
+        """
+        #print('test_update_meta_file_empty_date_list')
+        #expected results
+        return_exp = True
+        #log_exp = 'The dataframe is empty! No file will be written!'
+        # Test init
+        date_list = []
+        meta_key='meta.csv'
+        # Method execution
+        with self.assertLogs() as logm:
+            result = MetaProcess.update_meta_file(date_list, meta_key, self.s3_bucket_meta)
+            #log test after method execution
+            #self.assertIn(log_exp,logm.output[1])
+        #Test after method execution
+        self.assertEqual(return_exp, result)
+        
+        #cleanup after test
+        self.s3_bucket.delete_objects(
+            Delete={
+                'Objects':[
+                    {
+                        'Key': meta_key
+                    }
+                ]
+            }
+        )
+
+    def test_update_meta_file_meta_file_ok(self):
+        """
+            Tests the update_meta_file when the meta file is ok
+        """
+        #print('test_update_meta_file_meta_file_ok')
+
+        date_list_old = ['2021-04-12','2021-04-13']
+        date_list_new = ['2021-04-16','2021-04-17']
+        date_list_exp = date_list_old + date_list_new
+        proc_date_list_exp = [datetime.today().date()] * 4
+
+        # Test init
+        meta_key = 'meta.csv'
+        meta_content = (
+          f'{MetaProcessFormat.META_SOURCE_DATE_COL.value},'
+          f'{MetaProcessFormat.META_PROCESS_COL.value}\n'
+          f'{date_list_old[0]},'
+          f'{datetime.today().strftime(MetaProcessFormat.META_PROCESS_DATE_FORMAT.value)}\n'
+          f'{date_list_old[1]},'
+          f'{datetime.today().strftime(MetaProcessFormat.META_PROCESS_DATE_FORMAT.value)}'
+        )
+
+        self.s3_bucket.put_object(Body= meta_content,Key = meta_key)
+        # Method execution
+        MetaProcess.update_meta_file(date_list_new,meta_key,self.s3_bucket_meta)
+        # Read meta file
+        data = self.s3_bucket.Object(key=meta_key).get().get('Body').read().decode('utf-8')
+        out_buffer = StringIO(data)
+        df_meta_result = pd.read_csv(out_buffer)
+        date_list_result =list(df_meta_result[MetaProcessFormat.META_SOURCE_DATE_COL.value])
+        proc_date_list_result =list(
+            pd.to_datetime(df_meta_result[MetaProcessFormat.META_PROCESS_COL.value]).dt.date
+        )
+
+        #Test after method execution
+        self.assertEqual(date_list_exp, date_list_result)
+        self.assertEqual(proc_date_list_exp,proc_date_list_result)
+
+        #cleanup after test
+        self.s3_bucket.delete_objects(
+            Delete={
+                'Objects':[
+                    {
+                        'Key': meta_key
+                    }
+                ]
+            }
+        )
+
+
+    def test_update_meta_file_meta_file_wrong(self):
+        """
+            Tests the update_meta_file when the meta file is incorrect
+        """
+
+        #print('test_update_meta_file_meta_file_wrong')
+        #expected results
+        date_list_old = ['2021-04-12','2021-04-13']
+        date_list_new = ['2021-04-16','2021-04-17']
+        
+
+        # Test init
+        meta_key = 'meta.csv'
+        meta_content = (
+          f'wrong_column,{MetaProcessFormat.META_SOURCE_DATE_COL.value},'
+          f'{MetaProcessFormat.META_PROCESS_COL.value}\n'
+          f'{date_list_old[0]},'
+          f'{datetime.today().strftime(MetaProcessFormat.META_PROCESS_DATE_FORMAT.value)}\n'
+          f'{date_list_old[1]},'
+          f'{datetime.today().strftime(MetaProcessFormat.META_PROCESS_DATE_FORMAT.value)}'
+        )
+
+        self.s3_bucket.put_object(Body= meta_content,Key = meta_key)
+        # Method execution
+        with self.assertRaises(WrongMetaFileException):
+             MetaProcess.update_meta_file(date_list_new,meta_key,self.s3_bucket_meta)
+        
+        #cleanup after test
+        self.s3_bucket.delete_objects(
+            Delete={
+                'Objects':[
+                    {
+                        'Key': meta_key
+                    }
+                ]
+            }
+        )
+
+    def test_return_date_list_empty_date_list(self):
+        """
+        Tests the return_date_list method
+        when there are no dates to be returned
+        """
+
+        #print('test_return_date_list_empty_date_list')
+        # Expected results
+        min_date_exp = '2200-01-01'
+        date_list_exp = []
+        # Test init
+        meta_key = 'meta.csv'
+        meta_content = (
+          f'{MetaProcessFormat.META_SOURCE_DATE_COL.value},'
+          f'{MetaProcessFormat.META_PROCESS_COL.value}\n'
+          f'{self.dates[0]},{self.dates[0]}\n'
+          f'{self.dates[1]},{self.dates[0]}'
+        )
+        self.s3_bucket.put_object(Body=meta_content, Key=meta_key)
+        first_date = self.dates[0]
+        # Method execution
+        min_date_return, date_list_return = MetaProcess.return_date_list(first_date, meta_key,
+                                                                         self.s3_bucket_meta)
+        # Test after method execution
+        self.assertEqual(date_list_exp, date_list_return)
+        self.assertEqual(min_date_exp, min_date_return)
+        # Cleanup after test
+        self.s3_bucket.delete_objects(
+            Delete={
+                'Objects': [
+                    {
+                        'Key': meta_key
+                    }
+                ]
+            }
+        )
+
+if __name__ =="__main__":
+    unittest.main()
--- xetra/common/meta_process.py
+++ xetra/common/meta_process.py
@@ -24,10 +24,10 @@ class MetaProcess():
         :param: s3_bucket_meta: S3BucketConnector for the bucket with the meta file
         
         """
-         # Creating an empty DataFrame using the meta file column names
+        # Creating an empty DataFrame using the meta file column names
         df_new = pd.DataFrame(columns=[
-          MetaProcessFormat.META_SOURCE_DATE_COL.value,
-          MetaProcessFormat.META_PROCESS_COL.value])
+        MetaProcessFormat.META_SOURCE_DATE_COL.value,
+        MetaProcessFormat.META_PROCESS_COL.value])
         # Filling the date column with extract_date_list
         df_new[MetaProcessFormat.META_SOURCE_DATE_COL.value] = extract_date_list
         # Filling the processed column
@@ -48,5 +48,55 @@ class MetaProcess():
     
     
     @staticmethod
-    def return_date_list():
-        pass
+    def return_date_list(first_date: str, meta_key: str, s3_bucket_meta: S3BucketConnector):
+        """
+        Creating a list of dates based on the input first_date and the already
+        processed dates in the meta file
+
+        :param: first_date -> the earliest date Xetra data should be processed
+        :param: meta_key -> key of the meta file on the S3 bucket
+        :param: s3_bucket_meta -> S3BucketConnector for the bucket with the meta file
+
+        returns:
+          min_date: first date that should be processed
+          return_date_list: list of all dates from min_date till today
+        """
+
+        start = datetime.strptime(first_date,
+                                  MetaProcessFormat.META_DATE_FORMAT.value)\
+                                      .date() - timedelta(days=1)
+        today = datetime.today().date()
+        try:
+            # If meta file exists create return_date_list using the content of the meta file
+            # Reading meta file
+            df_meta = s3_bucket_meta.read_csv_to_df(meta_key)
+            # Creating a list of dates from first_date untill today
+            dates = [start + timedelta(days=x) for x in range(0, (today - start).days + 1)]
+            # Creating set of all dates in meta file
+            src_dates = set(pd.to_datetime(
+              df_meta[MetaProcessFormat.META_SOURCE_DATE_COL.value]
+              ).dt.date)
+            dates_missing = set(dates[1:]) - src_dates
+            if dates_missing:
+                # Determining the earliest date that should be extracted
+                min_date = min(set(dates[1:]) - src_dates) - timedelta(days=1)
+                # Creating a list of dates from min_date untill today
+                return_min_date = (min_date + timedelta(days=1))\
+                    .strftime(MetaProcessFormat.META_DATE_FORMAT.value)
+                return_dates = [
+                    date.strftime(MetaProcessFormat.META_DATE_FORMAT.value) \
+                        for date in dates if date >= min_date
+                        ]
+            else:
+                # Setting values for the earliest date and the list of dates
+                return_dates = []
+                return_min_date = datetime(2200, 1, 1).date()\
+                    .strftime(MetaProcessFormat.META_DATE_FORMAT.value)
+        except s3_bucket_meta.session.client('s3').exceptions.NoSuchKey:
+            # No meta file found -> creating a date list from first_date - 1 day untill today
+            return_min_date = first_date
+            return_dates = [
+              (start + timedelta(days=x)).strftime(MetaProcessFormat.META_DATE_FORMAT.value) \
+              for x in range(0, (today - start).days + 1)
+              ]
+        return return_min_date, return_dates
